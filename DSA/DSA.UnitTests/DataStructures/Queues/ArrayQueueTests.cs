e9b5d37a27561052b1340b97e11f5bf29d92fecc
Skyuzii <Skyuzii@yandex.ru>
2024-08-24 18:45:21 +0300

    add tests for array queue

---
+++ DSA/DSA.UnitTests/DataStructures/Queues/ArrayQueueTests.cs
@@ -0,0 +1,303 @@
+using DSA.Core.DataStructures.Queues;
+
+namespace DSA.UnitTests.DataStructures.Queues;
+
+public class ArrayQueueTests
+{
+    [Fact]
+    public void EnqueuingItemsOneByOne()
+    {
+        var queue = new ArrayQueue<int>();
+
+        var itemsCount = 10;
+
+        for (int i = 0; i < itemsCount; i++)
+        {
+            queue.Enqueue(i);
+        }
+
+        int trueCount = 0;
+        int lastItem = int.MinValue;
+
+        foreach (var item in queue)
+        {
+            if (lastItem > item) Assert.Fail();
+            lastItem = item;
+            trueCount++;
+        }
+
+        Assert.True(queue.Count == itemsCount && queue.Count == trueCount);
+    }
+
+    [Fact]
+    public void DequeuingAllItemsExceptOne()
+    {
+        var queue = new ArrayQueue<int>();
+
+        var itemsCount = 10;
+
+        for (int i = 0; i < itemsCount; i++)
+        {
+            queue.Enqueue(i);
+        }
+
+        int lastItem = int.MinValue;
+
+        for (int i = 0; i < itemsCount - 1; i++)
+        {
+            var item = queue.Dequeue();
+            if (lastItem > item)
+            {
+                Assert.Fail();
+            }
+
+            lastItem = item;
+        }
+
+        var trueCount = queue.Count();
+
+        Assert.True(queue.Count == 1 && trueCount == 1);
+    }
+    
+    [Fact]
+    public void DequeuingAllExceptOne()
+    {
+        var queue = new ArrayQueue<int>();
+
+        int itemCount = 500000;
+
+        for (int i = 0; i < itemCount; i++)
+        {
+            queue.Enqueue(i);
+        }
+
+        int lastItem = int.MinValue;
+        for (int i = 0; i < itemCount - 1; i++)
+        {
+            if (lastItem > queue.Dequeue()) Assert.Fail();
+        }
+
+        int trueCount = 0;
+
+
+        foreach (var item in queue)
+        {
+            trueCount++;
+        }
+
+        Assert.True(queue.Count == 1 && trueCount == 1);
+    }
+
+    [Fact]
+    public void InitializationWithZeroCapacityAndEnqueuingItemsAfterwards()
+    {
+        var queue = new ArrayQueue<int>(0);
+
+        int itemCount = 500000;
+
+        for (int i = 0; i < itemCount; i++)
+        {
+            queue.Enqueue(i);
+        }
+
+        int trueCount = 0;
+        int lastItem = int.MinValue;
+        foreach (var item in queue)
+        {
+            if (lastItem > item) Assert.Fail();
+            lastItem = item;
+            trueCount++;
+        }
+
+        Assert.True(queue.Count == itemCount
+                      && queue.Count == trueCount);
+    }
+
+    [Fact]
+    public void DequeuingAllItemsAndEnqueuingAgain()
+    {
+        var queue = new ArrayQueue<int>();
+
+        int itemCount = 500000;
+
+        for (int i = 0; i < itemCount; i++)
+        {
+            queue.Enqueue(i);
+        }
+
+        int lastItem = int.MinValue;
+        for (int i = 0; i < itemCount; i++)
+        {
+            if (lastItem > queue.Dequeue()) Assert.Fail();
+        }
+
+        bool countWasZero = queue.Count == 0;
+
+        for (int i = 0; i < itemCount; i++)
+        {
+            queue.Enqueue(i);
+        }
+
+        int trueCount = 0;
+        lastItem = int.MinValue;
+
+        foreach (var item in queue)
+        {
+            if (lastItem > item) Assert.Fail();
+            lastItem = item;
+            trueCount++;
+        }
+
+        Assert.True(queue.Count == itemCount
+                      && queue.Count == trueCount
+                      && countWasZero);
+    }
+
+    [Fact]
+    public void CheckIfContainedBeforeAndAfterDequeuing()
+    {
+        var queue = new ArrayQueue<int>();
+
+        int itemCount = 10000;
+
+        for (int i = 0; i < itemCount; i++)
+        {
+            if (queue.Contains(i)) Assert.Fail();
+            queue.Enqueue(i);
+            if (!queue.Contains(i)) Assert.Fail();
+        }
+
+        int lastItem = int.MinValue;
+        for (int i = 0; i < itemCount; i++)
+        {
+            var first = queue.Peek();
+            if (!queue.Contains(i)) Assert.Fail();
+            var dequeued = queue.Dequeue();
+            if (dequeued != first) Assert.Fail();
+            if (lastItem > dequeued) Assert.Fail();
+            if (queue.Contains(i)) Assert.Fail();
+            lastItem = first;
+        }
+
+        Assert.True(queue.Count == 0);
+    }
+
+    [Fact]
+    public void EnqueuingAfterClearingCollection()
+    {
+        var queue = new ArrayQueue<int>();
+
+        int itemCount = 500000;
+
+        for (int i = 0; i < itemCount; i++)
+        {
+            queue.Enqueue(i);
+        }
+
+        queue.Clear();
+
+        for (int i = 0; i < itemCount; i++)
+        {
+            queue.Enqueue(i);
+        }
+
+        int trueCount = 0;
+        int lastItem = int.MinValue;
+        foreach (var item in queue)
+        {
+            if (lastItem > item) Assert.Fail();
+            lastItem = item;
+            trueCount++;
+        }
+
+        Assert.True(queue.Count == itemCount && queue.Count == trueCount);
+    }
+
+    [Fact]
+    public void EnqueuingItemsAndCheckingIfIteratedInCorrectly()
+    {
+        var queue = new ArrayQueue<int>();
+
+        int itemCount = 500000;
+
+        for (int i = 0; i < itemCount; i++)
+        {
+            queue.Enqueue(i);
+        }
+
+        int trueCount = 0;
+        int itemNum = 0;
+        foreach (var item in queue)
+        {
+            if (itemNum++ != item) Assert.Fail();
+            trueCount++;
+        }
+
+        Assert.True(queue.Count == itemCount
+                      && queue.Count == trueCount);
+    }
+
+    [Fact]
+    public void ConvertingQueueToArray()
+    {
+        var queue = new ArrayQueue<int>();
+
+        int itemCount = 500000;
+
+        for (int i = 0; i < itemCount; i++)
+        {
+            queue.Enqueue(i);
+        }
+
+        var array = queue.ToArray();
+
+        int trueCount = 0;
+        for (int i = 0; i < itemCount; i++)
+        {
+            if (array[i] != i) Assert.Fail();
+            trueCount++;
+        }
+
+        Assert.True(queue.Count == itemCount
+                      && queue.Count == trueCount);
+    }
+
+    [Fact]
+    public void DequeuingHalfTheItemsAndEnquingTwiceAsMuch()
+    {
+        int itemCount = 500000;
+
+        var queue = new ArrayQueue<int>(itemCount);
+
+        for (int i = 0; i < itemCount; i++)
+        {
+            queue.Enqueue(i);
+        }
+
+        int lastItem = int.MinValue;
+        for (int i = 0; i < itemCount >> 1; i++)
+        {
+            if (lastItem > queue.Dequeue()) Assert.Fail();
+        }
+
+        int maxItem = itemCount + itemCount * 2;
+
+        for (int i = itemCount; i < maxItem; i++)
+        {
+            queue.Enqueue(i);
+        }
+
+        itemCount = maxItem - itemCount + itemCount / 2;
+
+        int trueCount = 0;
+        lastItem = int.MinValue;
+        foreach (var item in queue)
+        {
+            if (lastItem > item) Assert.Fail();
+            lastItem = item;
+            trueCount++;
+        }
+
+        Assert.True(queue.Count == trueCount && itemCount == trueCount);
+    }
+}
